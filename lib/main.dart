4f7941e1cc24cb7888e7293f44706796c13bfcfb
Admin <makwanadharmishtha44@gmail.com>
2023-01-18 15:56:21 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:pr11/screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => Screen10(),
+      },
+    ),
+  );
+}
